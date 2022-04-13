6d42da748e925b2d953acf2a1619c859c7c34eed
Victor Hugo <vrmq@gft.com>
2022-04-13 14:42:18 -0500

    fix: dont work add and edit function. Not redirect to new element when clicked save

--- src/app/heroes/pages/agregar/agregar.component.ts
+++ src/app/heroes/pages/agregar/agregar.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Heroe, Publisher } from '../../interfaces/heroes.interfaces';
+import { HeroesService } from '../../services/heroes.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-agregar',
@@ -8,9 +12,58 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AgregarComponent implements OnInit {
 
-  constructor() { }
+  publishers = [
+    {
+      id: 'DC Comics',
+      desc: 'DC - Comics'
+    },
+    {
+      id: 'Marvel Comics',
+      desc: 'Marvel - Comics'
+    }
+  ];
+
+  heroe: Heroe = {
+    superhero: '',
+    alter_ego: '',
+    characters: '',
+    first_appearance: '',
+    publisher: Publisher.DCComics,
+    alt_img: '',
+  }
+
+  constructor(
+    private heroesService: HeroesService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router
+    ) { }
 
   ngOnInit(): void {
+
+    this.activatedRoute.params
+    //retrive the data  and fills the forms for editing
+      .pipe(
+        switchMap( ({id}) => this.heroesService.getHeroePorId( id ))
+      )
+      .subscribe( heroe =>  this.heroe = heroe);
+  }
+
+  guardar() {
+    if( this.heroe.superhero.trim().length === 0){
+      return;
+    }
+
+    if( this.heroe.id) {
+      //update hero regisrter
+      this.heroesService.updateHeroe( this.heroe )
+        .subscribe( heroe => console.log('actualizando', heroe))
+    } else {
+      //create hero register
+      this.heroesService.updateHeroe( this.heroe )
+        .subscribe( heroe => {
+          this.router.navigate(['/heroes/editar', heroe.id])
+        })
+    }
   }
 
 }
